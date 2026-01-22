175daf26a26cac2979cc047c0e34c1c6e93be600
Matheus Afornali <afornalimatheus@gmail.com>
2026-01-22 00:41:31 -0300

    first commit :)

---
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function __invoke(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            
+            // Criar um token de autenticação (usando Sanctum)
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login realizado com sucesso',
+                'user' => $user,
+                'token' => $token,
+            ], 200);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['As credenciais fornecidas estão incorretas.'],
+        ]);
+    }
+}
